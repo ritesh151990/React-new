13234bb1f13dce71ff98a99c6b7c8a0ae62fe5ad
ritesh151990 <ritesh151990@gmail.com>
2022-03-04 18:34:21 +0530

    first react

---
+++ ExpenseItem.js
@@ -0,0 +1,14 @@
+function ExpenseItem(){
+    return (<div>
+        <div>
+        <h2>Expense item!</h2> 
+        <div>Food   Rs  10</div>
+        <div>Petrol Rs 100</div>
+        <div>Movies Rs 200</div>
+
+        </div>
+        </div>
+    );
+}
+
+export default ExpenseItem;
